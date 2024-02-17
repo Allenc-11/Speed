19aaf8c128aca78dc1deb4af2b9100511d31deb2
Allen <129023261+Allenc-11@users.noreply.github.com>
2024-02-17 02:34:42 -0500

    format fix

--- src/Speed.java
+++ src/Speed.java
@@ -17,23 +17,25 @@ import javax.swing.JOptionPane;
 public class Speed {
 	public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException, URISyntaxException{
 		try {
-		String currentDirectoryPath = System.getProperty("user.dir");
-		currentDirectoryPath = currentDirectoryPath.replace("\\src", "");
-		Sound bgm = new Sound(new File(currentDirectoryPath, "bin/BGM.wav")); // create new sound
-		
-		GetScreenDimension getSize = new GetScreenDimension(); // create new GetScreenDimesnsion
-		JFrame window = new JFrame("Speed"); // create new JFrame
-		Dimension screen = getSize.getScreenSize(); // create new dimension
-		
-		window.setSize((int) screen.getWidth(), (int) screen.getHeight());
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		MainInterface main = new MainInterface(window, window.getWidth(), window.getHeight()); // create new Main Interface
-		main.show(); // show main interface
-		window.setVisible(true); // set window to visible
-		window.setExtendedState(JFrame.MAXIMIZED_BOTH); //set to full screen
-		window.setResizable(false);//set the frame to not resizable
-		bgm.playLoop(); // play bgm
+			String currentDirectoryPath = System.getProperty("user.dir");
+			currentDirectoryPath = currentDirectoryPath.replace("\\src", "");
+			File f = new File("C:/Speed.java");
+			System.out.println(f.exists());
+			Sound bgm = new Sound(new File(currentDirectoryPath, "bin/BGM.wav")); // create new sound
+			
+			GetScreenDimension getSize = new GetScreenDimension(); // create new GetScreenDimesnsion
+			JFrame window = new JFrame("Speed"); // create new JFrame
+			Dimension screen = getSize.getScreenSize(); // create new dimension
+			
+			window.setSize((int) screen.getWidth(), (int) screen.getHeight());
+			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			MainInterface main = new MainInterface(window, window.getWidth(), window.getHeight()); // create new Main Interface
+			main.show(); // show main interface
+			window.setVisible(true); // set window to visible
+			window.setExtendedState(JFrame.MAXIMIZED_BOTH); //set to full screen
+			window.setResizable(false);//set the frame to not resizable
+			bgm.playLoop(); // play bgm
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e, null, 0);
 		}
